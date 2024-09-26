dd9364b7007c7bebc0da13fe4ad7897452f7aed1
sujan-thapa <sujanthapamagar960@gmail.com>
2024-09-26 18:43:46 -0400

    git init and add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sujan-thapa/w4_csdd_1008_fall24
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	print("Hello, World!\n")
+	fmt.Println("Hello, World!")
+}
